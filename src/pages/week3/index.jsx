eb4687b70b821ffedb1732a756ed2585276827cd
Maojin-juan <edwardelric199887@gmail.com>
2025-01-15 23:25:21 +0800

    完成 將 Dialog 移至 Week3

--- src/pages/week3/index.jsx
+++ src/pages/week3/index.jsx
@@ -1,23 +1,35 @@
 import { useState } from "react";
 
+import { Dialog, DialogTrigger, Button } from "@/components/ui";
+
 import Login from "@/components/Login";
-import CreateProduct from "@/components/CreateProduct";
+import ProductHandle from "@/components/ProductHandle";
+
+import ProductModal from "@/components/ProductModal";
 
 function Week3() {
   const [isAuth, setIsAuth] = useState(false);
   const [products, setProducts] = useState([]);
-  const [tempProduct, setTempProduct] = useState(null);
 
   return (
     <>
       {isAuth ? (
-        <div className="container mx-auto">
-          <div className="mx-auto mt-12 flex gap-6">
-            <CreateProduct
-              products={products}
-              setTempProduct={setTempProduct}
-            />
-          </div>
+        <div className="container mx-auto mt-12 gap-6">
+          <Dialog>
+            <div className="flex justify-end">
+              <DialogTrigger asChild>
+                <Button
+                  variant="outline"
+                  className="rounded-full px-8 py-2"
+                  size="lg"
+                >
+                  建立新的產品
+                </Button>
+              </DialogTrigger>
+            </div>
+            <ProductHandle products={products} />
+            <ProductModal title="建立新的產品" />
+          </Dialog>
         </div>
       ) : (
         <Login setIsAuth={setIsAuth} setProducts={setProducts} />
