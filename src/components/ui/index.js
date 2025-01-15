9a6d99b6d28981b2fdf4c89bea7678e9ead29e9b
Maojin-juan <edwardelric199887@gmail.com>
2025-01-15 15:38:32 +0800

    完成 加入 checkbox 跟 textarea 插件並將其整合在 UI 資料夾中以便提取使用

---
+++ src/components/ui/index.js
@@ -0,0 +1,29 @@
+export {
+  Dialog,
+  DialogTrigger,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+export {
+  Table,
+  TableHeader,
+  TableRow,
+  TableHead,
+  TableBody,
+  TableCell,
+} from "@/components/ui/table";
+
+export { Label } from "@/components/ui/label";
+
+export { Input } from "@/components/ui/input";
+
+export { Button } from "@/components/ui/button";
+
+export { Textarea } from "@/components/ui/textarea";
+
+export { Checkbox } from "@/components/ui/checkbox";
