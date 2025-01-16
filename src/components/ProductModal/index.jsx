ba93099dfd2a549e7e12f4ddf75d84425308f26b
Maojin-juan <edwardelric199887@gmail.com>
2025-01-16 15:52:00 +0800

    完成 完成建立產品功能

--- src/components/ProductModal/index.jsx
+++ src/components/ProductModal/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 import {
   DialogClose,
@@ -10,7 +11,32 @@ import {
 } from "@/components/ui";
 import ProductForm from "./ProductForm";
 
-function ProductModal({ title, modalType }) {
+import { productAPI } from "@/services/product";
+
+const initialFormData = {
+  id: "",
+  imageUrl: "",
+  title: "",
+  category: "",
+  unit: "",
+  origin_price: "",
+  price: "",
+  description: "",
+  content: "",
+  is_enabled: false,
+  imagesUrl: [],
+};
+
+function ProductModal({ title, modalType, setProducts }) {
+  const [formData, setFormData] = useState(initialFormData);
+
+  const onSubmit = async () => {
+    await productAPI.updateProductData(formData.id, formData, modalType);
+    const updatedProducts = await productAPI.getProducts();
+
+    setProducts(updatedProducts.products);
+  };
+
   return (
     <DialogContent className="h-5/6 gap-0 rounded-lg p-0">
       <DialogHeader className="justify-center rounded-t-lg border border-b p-6">
@@ -19,22 +45,22 @@ function ProductModal({ title, modalType }) {
           This is a hidden description to avoid warnings.
         </DialogDescription>
       </DialogHeader>
-      <ProductForm />
+      {modalType === "delete" ? (
+        <div>Delete</div>
+      ) : (
+        <ProductForm formData={formData} setFormData={setFormData} />
+      )}
       <DialogFooter className="flex flex-row items-center rounded-b-lg border border-t p-6">
         <DialogClose asChild>
           <Button type="button" variant="secondary">
             取消
           </Button>
         </DialogClose>
-        {modalType === "delete" ? (
-          <DialogClose asChild>
-            <Button type="button">刪除</Button>
-          </DialogClose>
-        ) : (
-          <DialogClose asChild>
-            <Button type="button">確認</Button>
-          </DialogClose>
-        )}
+        <DialogClose asChild>
+          <Button type="button" onClick={onSubmit}>
+            {modalType === "delete" ? "刪除" : "確認"}
+          </Button>
+        </DialogClose>
       </DialogFooter>
     </DialogContent>
   );
@@ -43,6 +69,7 @@ function ProductModal({ title, modalType }) {
 ProductModal.propTypes = {
   title: PropTypes.string,
   modalType: PropTypes.string,
+  setProducts: PropTypes.func,
 };
 
 export default ProductModal;
